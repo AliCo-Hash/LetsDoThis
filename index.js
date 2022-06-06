2351376b5ba56e2ad29252227ab05769c0996e1f
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-06-06 01:15:54 +0100

    organiser can see start list

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ async function getData() {
   const eventData = await res.json();
   return eventData;
 }
+
+async function getStartlist(eventId) {
+  const namesArr = [];
+  const dataSet = await getData();
+
+  dataSet.forEach(data => {
+    if (eventId == data.eventId && data.status == "CONFIRMED") {
+      namesArr.push(`${data.firstName} ${data.lastName}`);
+    }
+  });
+  return namesArr.join("\n");
+}
+
+async function printNames() {
+  const namesOutput = document.getElementById("names");
+  const eventInput = document.getElementById("eventid").value;
+  const printedName = getStartlist(eventInput);
+  namesOutput.innerText = await printedName;
+}
+
+document.getElementById("eventBtn").addEventListener("click", printNames);
